a27ceae0f1985c033be10742601de3968dc16d16
neil.yinz <neil.yinz@gmail.com>
2013-02-11 13:58:03 +0000

    完美了一下代码

--- yunarmap.js
+++ yunarmap.js
@@ -1,8 +1,7 @@
 document.write("<scri"+"pt type='text/javascript' src='http://maps.googleapis.com/maps/api/js?key=AIzaSyDESpX0WV4KVfHtiq8aBCtXzPTmrAhuZ4E&sensor=true'></scr"+"ipt>"); 
 
-function detectBrowser() {
+function detectBrowser(mapdiv) {
 	var useragent = navigator.userAgent;
-	var mapdiv = document.getElementById("yunar_map");
 
 	if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1 ) {
 		mapdiv.style.width = '100%';
@@ -13,11 +12,12 @@ function detectBrowser() {
 	}
 }
 function initialize() {
-	detectBrowser();
+	var mapdiv = document.getElementById("yunar_map");
+	detectBrowser(mapdiv);
 	var mapOptions = {
 		center: new google.maps.LatLng(39.92, 116.46),
 		zoom: 12,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
-	var map = new google.maps.Map(document.getElementById("yunar_map"), mapOptions);
+	var map = new google.maps.Map(mapdiv, mapOptions);
 }
