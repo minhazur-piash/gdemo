0fed0b230391729d8be7e15019c1f378c7bb1ea6
neil.yinz@gmail.com <neil.yinz@gmail.com@db9ce267-d169-4e93-6056-6383b30cfde7>
2013-02-16 15:24:30 +0000

    新版地图

--- trunk/yunarmap.js
+++ trunk/yunarmap.js
@@ -1,10 +1,47 @@
-﻿//document.write("<scri"+"pt type='text/javascript' src='http://maps.googleapis.com/maps/api/js?key=AIzaSyDESpX0WV4KVfHtiq8aBCtXzPTmrAhuZ4E&sensor=true'></scr"+"ipt>"); 
+﻿
+$(document).ready(function(){
+	if(google.maps.Map) {
+		$("#error").append("<br>地图api连接成功</br>");
+	} else {
+		$("#error").append("<br>地图api连接失败</br>");
+		$("#error").css("color","red");
+		return;
+	}
+	var isPhone = detectBrowser();
+	var mapOptions = {
+		center: new google.maps.LatLng(39.92, 116.46),
+		zoom: 12,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	var mapdiv = document.getElementById("yunar_map");
+	if(isPhone == 0) {
+		mapdiv.style.width = '640px';
+		mapdiv.style.height = '480px';
+	} else {
+		//mapdiv.style.position = 'absolute';
+		//mapdiv.style.top = '0px';
+		//mapdiv.style.left = '0px';
+		mapdiv.style.width = '100%';
+		mapdiv.style.height = '100%';
+	}
+	var map = new google.maps.Map(mapdiv, mapOptions);
+	if(isPhone == 0) {
+		var sizeControlDiv = document.createElement('div');
+		var sizeControl = new sizeButton(sizeControlDiv, map);
+		map.controls[google.maps.ControlPosition.TOP_RIGHT].push(sizeControlDiv);
 
-var isPhone;
+	}
+	var inputControlDiv = document.createElement('div');
+	var inputControl = new inputButton(inputControlDiv, map);
+	map.controls[google.maps.ControlPosition.TOP_RIGHT].push(inputControlDiv);
+	var nameControlDiv = document.createElement('div');
+	var nameControl = new nameText(nameControlDiv, map);
+	map.controls[google.maps.ControlPosition.TOP_LEFT].push(nameControlDiv);
+});
 
 function detectBrowser() {
 	var useragent = navigator.userAgent;
-
+	var isPhone;
 	if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1 ) {
 		//mapdiv.style.width = '100%';
 		//mapdiv.style.height = '100%';
@@ -14,7 +51,9 @@ function detectBrowser() {
 		//mapdiv.style.height = '480px';
 		isPhone = 0;
 	}
+	return isPhone;
 }
+
 
 // Define a property to hold the Size state.
 sizeButton.prototype.size_ = 0;
@@ -78,19 +117,23 @@ function sizeButton(controlDiv, map) {
   });
 }
 
-//var markersArray = [];
-
-function addMarker(map, location) {
-	var infowindow = new google.maps.InfoWindow({
-		content: '仓库'
-	});
+function addMarker(map, location, infowindow) {
+	var ckName = prompt("请输入仓库名","仓库1");
+	if(ckName==null||ckName=="") {
+		return;
+	}
+	infowindow.setContent(ckName+"<p><button>认证</button> <button>删除</button></p>");
+	//infowindow.setPosition(location);
   var marker = new google.maps.Marker({
     position: location,
+	//icon: "A.png",
     map: map
   });
   marker.setMap(map);
+  infowindow.open(map,marker);
   //markersArray.push(marker);
 	google.maps.event.addListener(marker, 'click', function() {
+	  infowindow.setContent(ckName+"<p><button>认证</button> <button>删除</button></p>");
 	  infowindow.open(map,marker);
 	});
 }
@@ -98,7 +141,10 @@ function addMarker(map, location) {
 // Define a property to hold the state.
 inputButton.prototype.state_ = 0;
 inputButton.prototype.listener_;
-
+inputButton.prototype.infowindow_;
+// = new google.maps.InfoWindow({
+//    content: contentString
+//});
 // Define setters and getters for this property.
 inputButton.prototype.getState = function() {
   return this.state_;
@@ -114,6 +160,14 @@ inputButton.prototype.getListener = function() {
 
 inputButton.prototype.setListener = function(listener) {
 	this.listener_ = listener;
+}
+
+inputButton.prototype.getInfoWindow = function() {
+	return this.infowindow_;
+}
+
+inputButton.prototype.setInfoWindow = function(infowindow) {
+	this.infowindow_ = infowindow;
 }
 
 function inputButton(controlDiv, map) {
@@ -149,7 +203,10 @@ function inputButton(controlDiv, map) {
 		controlText.innerHTML = '<strong>取消录入</strong>';
 		control.setState(1);
 		var listener = google.maps.event.addListenerOnce(map, 'click', function(event) {
-			addMarker(map, event.latLng);
+			if(control.getInfoWindow()==null) {
+				control.setInfoWindow(new google.maps.InfoWindow());
+			}
+			addMarker(map, event.latLng, control.getInfoWindow());
 			controlText.innerHTML = '<strong>录入</strong>';
 			control.setState(0);
 		});
@@ -162,116 +219,29 @@ function inputButton(controlDiv, map) {
   });
 }
 
-function loadMap() {
-	detectBrowser();
-	var mapOptions = {
-		center: new google.maps.LatLng(39.92, 116.46),
-		zoom: 12,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
-	var mapdiv = document.getElementById("yunar_map");
-	if(isPhone == 0) {
-		mapdiv.style.width = '640px';
-		mapdiv.style.height = '480px';
-	} else {
-		//mapdiv.style.position = 'absolute';
-		//mapdiv.style.top = '0px';
-		//mapdiv.style.left = '0px';
-		mapdiv.style.width = '100%';
-		mapdiv.style.height = '100%';
-	}
-	var map = new google.maps.Map(mapdiv, mapOptions);
-	if(isPhone == 0) {
-		var sizeControlDiv = document.createElement('div');
-		var sizeControl = new sizeButton(sizeControlDiv, map);
-		map.controls[google.maps.ControlPosition.TOP_RIGHT].push(sizeControlDiv);
-		var inputControlDiv = document.createElement('div');
-		var inputControl = new inputButton(inputControlDiv, map);
-		map.controls[google.maps.ControlPosition.TOP_RIGHT].push(inputControlDiv);
+function nameText(controlDiv, map) {
+	
+  // Set CSS styles for the DIV containing the control
+  // Setting padding to 5 px will offset the control
+  // from the edge of the map.
+  controlDiv.style.padding = '5px';
 
-	}
+  // Set CSS for the control border.
+  var controlUI = document.createElement('div');
+  controlUI.style.backgroundColor = 'white';
+  controlUI.style.borderStyle = 'solid';
+  controlUI.style.borderWidth = '1px';
+  controlUI.style.cursor = 'pointer';
+  controlUI.style.textAlign = 'center';
+  controlUI.title = 'Click to input user data';
+  controlDiv.appendChild(controlUI);
 
+  // Set CSS for the control interior.
+  var controlText = document.createElement('div');
+  controlText.style.fontFamily = 'Arial,sans-serif';
+  controlText.style.fontSize = '13px';
+  controlText.style.paddingLeft = '4px';
+  controlText.style.paddingRight = '4px';
+  controlText.innerHTML = '<strong>用户:</strong>游客';
+  controlUI.appendChild(controlText);
 }
-
-function read(path) {
-	var content = "";
-	try{
-			var fso = new ActiveXObject("Scripting.FileSystemObject");
-			var reader = fso.openTextFile(path, 1);
-			while(!reader.AtEndofStream) {
-					content += reader.readline();
-					content += "\n";
-			}
-			// close the reader
-			reader.close();
-	}catch(e){
-			if(document.implementation && document.implementation.createDocument){
-					try {
-							netscape.security.PrivilegeManager.enablePrivilege('UniversalXPConnect');
-							var lf = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
-							lf.initWithPath(path);
-							if (lf.exists() == false) {
-									alert("File does not exist");
-							}
-
-							var fis = Components.classes["@mozilla.org/network/file-input-stream;1"].createInstance(Components.interfaces.nsIFileInputStream);
-							fis.init(lf, 0x01, 00004, null);
-							var sis = Components.classes["@mozilla.org/scriptableinputstream;1"].createInstance(Components.interfaces.nsIScriptableInputStream);
-							sis.init(fis);
-							var converter = Components.classes["@mozilla.org/intl/scriptableunicodeconverter"].createInstance(Components.interfaces.nsIScriptableUnicodeConverter);
-							content = converter.ConvertToUnicode(sis.read(sis.available()));
-							converter.charset = "UTF-8";
-					}
-					catch (e) {
-							alert("Mozilla Firefox read local file error: \n" + e);
-					}
-			}
-	}
-	return content;
-}
-function write(arrstr,path){
-	try{
-	   var fso  = new ActiveXObject("Scripting.FileSystemObject");
-	   var fh = fso.CreateTextFile(path, true);
-	   fh.WriteLine(arrstr);
-	   fh.Close();
-	}catch(e){
-		try {
-			var pm = netscape.security.PrivilegeManager;
-			pm.enablePrivilege('UniversalXPConnect');
-		}catch(e)
-		{
-			alert("!!被浏览器拒绝！\n请在浏览器地址栏输入'about:config'并回车\n然后将'signed.applets.codebase_principal_support'设置为'true'");
-		}
-		var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
-
-	  // alert("create file object");
-		 if(file instanceof Components.interfaces.nsILocalFile)
-			file.initWithPath(path);
-		else
-		{
-			alert("erro");
-			return;
-		}
-		//alert("init file object");
-		var outputStream = Components.classes["@mozilla.org/network/file-output-stream;1"].createInstance(Components.interfaces.nsIFileOutputStream);
-		//alert("create fout object");
-
-		try
-		{
-			 outputStream.init(file,0x04|0x08|0x20,420,0);
-		}catch(e){alert(e);}
-		try
-		{
-			for(var i=0; i < arrstr.length; i++)
-			{
-				arrstr[i]+="\r\n";
-				outputStream.write(arrstr[i],arrstr[i].length);
-			}
-		}catch(e){alert(e);};
-		outputStream.close();
-	}
-}
-
-
-//google.maps.event.addDomListener(window, 'load', initialize);
